e2f8adf3893169b5e71da80bde9885a6fa2c9306
ankisinha3005 <ankit.sinha3005@gmail.com>
2019-04-06 09:57:32 +0530

    Obtained Co-ordinate of the place using mapBox api and geocoding

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const request=require('request')
+const geocode=require('./utilities/geocode.js')
 
 const url='https://api.darksky.net/forecast/132f42797fa2810a1722a584ec6fe6f4/37.8267,-122.4233'
 
 request({url:url,json:true},(error,response) => {
    if(error){
- console.log('There are some error')
+ console.log('Unable to Connect to the server')
  }
    else if(response.body.error) {
      console.log('No response received')
@@ -13,14 +14,18 @@ request({url:url,json:true},(error,response) => {
  }
 
  })
- // to get the co-ordinate of the system
- const geoUrl='https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiYW5raXRzaW5oYTMwMDUiLCJhIjoiY2p1NHhweW4wMHppMjQ0dGFvMnZ0cmxvayJ9.zKunhgyPFqtT1jsHQGtR9A'
+ // request to get the co-ordinate of the place using geocoding mapbox api
  
+ 
+// default 
+ geocode('Guwahati', (error, data) => {
+    console.log('Error', error)
+    console.log('Data', data)
+})
 
 
 
-
-
+ 
 
 
 
---
+++ utilities/geocode.js
@@ -0,0 +1,21 @@
+const request=require('request')
+
+const getGeoCode =(address,callback) => {
+ const geoUrl='https://api.mapbox.com/geocoding/v5/mapbox.places/'+ address + '.json?access_token=pk.eyJ1IjoiYW5raXRzaW5oYTMwMDUiLCJhIjoiY2p1NHhweW4wMHppMjQ0dGFvMnZ0cmxvayJ9.zKunhgyPFqtT1jsHQGtR9A'
+        
+    request({url:geoUrl,json:true},(error,response) => {
+     if(error){
+        callback('Unable to Connect the Server !',undefined)
+     }else if (response.body.features.length===0){
+        callback('No such location found',undefined)
+     }else{
+        callback(undefined,{
+           latitude: response.body.features[0].center[0],
+           longitude: response.body.features[0].center[1],
+           location: response.body.features[0].place_name
+       })
+     }
+   
+    })
+}
+    module.exports=getGeoCode
